1697f7e078ef9e6a53f9619688a29a4ab58f0a59
Mustafa Fahimy <mustafafahimy@gmail.com>
2022-06-14 01:37:59 +0430

    added functionalities

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-const menu = document.querySelector(".nav-links");
-const container = document.querySelector(".nav_container");
-const menuBtn = document.querySelector("#open-menu-btn");
-const closeBtn = document.querySelector("#close-menu-btn");
+const menu = document.querySelector('.nav-links');
+const container = document.querySelector('.nav_container');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+const list = document.querySelectorAll('.item');
 
 menuBtn.addEventListener('click', () => {
   container.classList.toggle('active');
-  menu.style.display = "flex";
-  menuBtn.style.display = "none";
-  closeBtn.style.display = "inline";
-})
+  menu.style.display = 'flex';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'inline';
+});
 
 const closeNav = () => {
-    menu.style.display = "none";
-    container.classList.toggle('nav_container');
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline";
-}
+  menu.style.display = 'none';
+  container.classList.toggle('nav_container');
+  closeBtn.style.display = 'none';
+  menuBtn.style.display = 'inline';
+};
 
-closeBtn.addEventListener('click', closeNav)
+closeBtn.addEventListener('click', closeNav);
+
+const toggleMenu = () => {
+  if (document.documentElement.clientWidth <= 767) {
+    container.classList.toggle('active');
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline';
+    closeBtn.style.display = 'none';
+  }
+};
+
+list.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenu);
+});
